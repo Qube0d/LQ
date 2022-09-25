6fdc49fc9bc2b3727af18d379e1c262124ce6fdd
Qube0d <smetacometa@gmail.com>
2022-09-25 18:49:12 +0300

    first commit

---
+++ spec/search.js
@@ -0,0 +1,57 @@
+const { expect } = require('@playwright/test');
+
+exports.SearchPage = class SearchPage {
+    constructor(page) {
+        this.page = page;
+        this.searchField = page.locator('.small');
+        this.tasksBtn = page.locator('#search-types >> text=Issues');
+        this.newsBtn = page.locator('#search-types >> text=News');
+        this.changesBtn = page.locator('text=Changesets');
+        this.pageWikiBtn = page.locator('text=Wiki pages');
+        this.messageBtn = page.locator('text=Messages');
+        this.redPlugBtn = page.locator('text=Redmine plugins');
+    }
+
+    async goto() {
+        await this.page.goto('https://www.redmine.org/');
+    }
+
+    async getStart() {
+        await this.searchField.click();
+        await this.searchField.press('Enter');
+        await this.tasksBtn.isVisible();
+        await this.newsBtn.isVisible();
+        await this.changesBtn.isVisible();
+        await this.pageWikiBtn.isVisible();
+        await this.messageBtn.isVisible();
+        await this.redPlugBtn.isVisible();
+    }
+    
+    async getSearchFielterTest() {
+        await this.searchField.click();
+        await this.searchField.fill('Download');
+        await this.searchField.press('Enter');
+        // await this.page.screenshot({ path: 'img/screenshot12.jpeg' });
+        await this.tasksBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot13.jpeg' });
+        await this.newsBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot14.jpeg' });
+        await this.changesBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot15.jpeg' });
+        await this.pageWikiBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot16.jpeg' });
+        await this.messageBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot17.jpeg' });
+        await this.redPlugBtn.click();
+        // await this.page.screenshot({ path: 'img/screenshot18.jpeg' });
+    }
+
+
+    async pageObjectModel() {
+        await this.getStart();
+        await this.getSearchFielterTest();
+  }
+
+
+}
+
